7170e6e2294aa9f5d2e47e7af0b4d9dcd5eadad2
Kevin Qian <yanxiang.qian@gmail.com>
2015-09-16 17:26:55 +0800

    ch2

---
+++ from.scratch/my_set.py
@@ -0,0 +1,20 @@
+class Set(object):
+    """My own Set class"""
+    def __init__(self, values=None):
+        self.dict = {}
+        if values is not None:
+            for value in values:
+                self.add(value)
+
+    def __repr__(self):
+        return "Set: " + str(self.dict.keys())
+
+    def add(self, value):
+        self.dict[value] = True
+
+    def contains(self, value):
+        return value in self.dict
+
+    def remove(self, value):
+        del self.dict[value]
+        
