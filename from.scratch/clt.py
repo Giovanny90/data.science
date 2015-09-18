b4dfbe30d4bc075f80958d57fb7671a8ea49bf0b
Kevin Qian <yanxiang.qian@gmail.com>
2015-09-18 10:24:17 +0800

    distributions

---
+++ from.scratch/clt.py
@@ -0,0 +1,39 @@
+# Central Limit Thoerem
+# Plot histogram
+
+from collections import Counter
+from distributions import normal_cdf
+import matplotlib.pyplot as plt
+import random, math
+
+
+def bernoulli_trial(p):
+    return 1 if random.random() < p else 0
+
+
+def binomial(n, p):
+    return sum(bernoulli_trial(p) for _ in range(n))
+
+
+def make_hist(p, n, num_points):
+
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8,
+            color='0.75')
+
+    mu = p * n
+    sigma = math.sqrt(n * p * (1 - p))
+
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma)
+          - normal_cdf(i - 0.5, mu, sigma)
+          for i in xs]
+    plt.plot(xs, ys)
+    plt.title("Binomial Dist. vs. Normal Approximation")
+    plt.show()
+
+make_hist(0.75, 100, 10000)
