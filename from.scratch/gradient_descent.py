fa3ae6e40d18055d36f5158ae92958b11564557a
Kevin Qian <yanxiang.qian@gmail.com>
2015-11-30 17:03:43 +0800

    from scratch

--- from.scratch/gradient_descent.py
+++ from.scratch/gradient_descent.py
@@ -32,12 +32,12 @@ derivative_estimate = partial(difference_quotient, square, h=0.00001)
 
 # plot to show they're basically the same
 import matplotlib.pyplot as plt
-x = range(-10, 10)
-plt.title("Actual Derivatives vs. Estimates")
-plt.plot(x, map(derivative, x), 'rx', label="Actual")
-plt.plot(x, map(derivative_estimate, x), 'b+', label="Estimate")
-plt.legend(loc=9)
-#plt.show()
+# x = range(-10, 10)
+# plt.title("Actual Derivatives vs. Estimates")
+# plt.plot(x, map(derivative, x), 'rx', label="Actual")
+# plt.plot(x, map(derivative_estimate, x), 'b+', label="Estimate")
+# plt.legend(loc=9)
+# plt.show()
 
 
 def partial_difference_quotient(f, v, i, h):
@@ -63,20 +63,17 @@ def sum_of_squares_gradient(v):
 
 
 # pick a random starting point
-v = [random.randint(-10, 10) for i in range(3)]
+# v = [random.randint(-10, 10) for i in range(3)]
 
-tolerance = 0.0000001
+# tolerance = 0.0000001
 
-while True:
-    gradient = sum_of_squares_gradient(v)
-    next_v = step(v, gradient, -0.01)
-    if distance(next_v, v) < tolerance:
-        break
-    v = next_v
-    #print v
+# while True:
+#     gradient = sum_of_squares_gradient(v)
+#     next_v = step(v, gradient, -0.01)
+#     if distance(next_v, v) < tolerance:
+#         break
+#     v = next_v
 
-
-step_sizes = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
 
 def safe(f):
 	"""return a new function that's the same as f,
@@ -87,3 +84,90 @@ def safe(f):
 		except:
 			return float('inf')
 	return safe_f
+
+# Put it all together
+def minimize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+    """use gradient descent to find theta that minimize target function."""
+
+    step_sizes = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001]
+
+    theta = theta_0
+    target_fn = safe(target_fn)
+    value = target_fn(theta)
+
+    while True:
+        gradient = gradient_fn(theta)
+        next_thetas = [step(theta, gradient, -step_size)
+                       for step_size in step_sizes]
+        # choose the one that minimizes the error function
+        next_theta = min(next_thetas, key=target_fn)                       
+        next_value = target_fn(next_theta)
+
+        # stop if we're "converging"
+        if abs(value - next_value) < tolerance:
+            return theta
+        else:
+            theta, value = next_theta, next_value
+
+
+def negate(f):
+    """return a function that for any input x return -f(x)"""
+    return lambda *args, **kwargs: -f(*args, **kwargs)
+
+def negate_all(f):
+    """the same when f returns a list of numbers"""
+    return lambda *args, **kwargs: [-y for y in f(*args, **kwargs)]
+
+def maximize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+
+    return minimize_batch(negate(target_fn),
+                          negate_all(gradient_fn),
+                          theta_0,
+                          tolerance) 
+
+
+# Stochastic gradient descent
+def in_random_order(data):
+    """generator that returns the elements of data in random order."""
+    indexes = [i for i, _ in enumerate(data)]
+    random.shuffle(indexes)
+    for i in indexes:
+        yield data[i]
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+
+    data = zip(x, y)
+    theta = theta_0
+    alpha = alpha_0
+    min_theta, min_value = None, float("inf")
+    iterations_with_no_improvement = 0
+
+    # if we ever go 100 iterations with no improvement, stop
+    while iterations_with_no_improvement < 100:
+        value = sum(target_fn(x_i, y_i, theta) for x_i, y_i in data)
+        
+        if value < min_value:
+            # if we've found a new minimum, remember it
+            # and go back to the original step size 
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            # otherwise we're not improving, so try shrinking the step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        # and take a gradient step for each of the data points
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
+
+def maximize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    return minimize_stochastic(negate(target_fn),
+                               negate_all(gradient_fn),
+                               x,
+                               y,
+                               theta_0,
+                               alpha_0)
